1935e5b7f00361c07d7087fd3c73fdb21f2408e3
salsh <salsh@Sally.lan>
2025-01-29 20:58:39 -0500

    implemented add()

--- src/demo1/DemoTest5.java
+++ src/demo1/DemoTest5.java
@@ -2,7 +2,18 @@ package demo1;
 
 public class DemoTest5 {
 	public static void main(String[] args) {
-		System.out.println("Java");
+		System.out.println("Sally");
+
+		int a = 10;
+		int b = 20;
+
+		int result = a + b;
+
+		System.out.println(result);
+	}
+
+	public void add(int a, int b) {
+		System.out.println(a + b);
 	}
 
 }
